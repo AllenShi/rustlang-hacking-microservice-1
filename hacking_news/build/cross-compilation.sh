aa4ed81c162a5815981c5a64a3d7f89dd81b0138
ali_shijinliang <ali_shijinliang@xbotech.com>
2020-12-01 01:30:45 +0800

    fix script error

--- hacking_news/build/cross-compilation.sh
+++ hacking_news/build/cross-compilation.sh
@@ -5,4 +5,4 @@ WORKSPACE_DIR=$(dirname $CUR_DIR)
 echo $WORKSPACE_DIR
 builder="docker run --rm -it -v $WORKSPACE_DIR:/home/rust/src ekidd/rust-musl-builder"
 $builder cargo build --release
-ls -lh target/x86_64-unknown-linux-musl/release/hacking_news_app
+ls -lh $WORKSPACE_DIR/target/x86_64-unknown-linux-musl/release/hacking_news_app
